f99f9f15cdcd88679ef2d11c003c3a33e58d0de1
Munenori Oizumi <oizumi@be.to>
2019-12-23 09:48:38 -0800

    BSS section is secured. Stack is added at the end.

--- RISCV_Emulator.cc
+++ RISCV_Emulator.cc
@@ -5,10 +5,11 @@
 #include <vector>
 #include <fstream>
 #include <elf.h>
+#include <cstring>
 
-// Code
-uint8_t mem[256];
-
+constexpr int kUnitSize = 1024 * 1024; // 1 MB
+constexpr int kInitialSize = 4 * kUnitSize; // 1 MB
+constexpr int kStackSize = 4 * kUnitSize; // 4 MB
 constexpr int kMaxBinarySize = 1024 * 1024 * 1024; // 1 GB
 
 std::vector<uint8_t> readFile(std::string filename)
@@ -22,7 +23,7 @@ std::vector<uint8_t> readFile(std::string filename)
   size = file.tellg();
   file.seekg(0, std::ios::beg);
   if (size > kMaxBinarySize || size <= 0) {
-    std::cout << "File size = " << size << "." << std::endl;
+    std::cerr << "File size = " << size << "." << std::endl;
     exit(-1);
   }
 
@@ -66,32 +67,87 @@ bool isRightElf(Elf32_Ehdr *ehdr) {
   return true;
 }
 
-typedef void (*func_t)();
+Elf32_Ehdr *get_ehdr(std::vector<uint8_t> &program) {
+  Elf32_Ehdr *ehdr = (Elf32_Ehdr *) reinterpret_cast<Elf32_Ehdr *>(program.data());
+  return ehdr;
+}
 
-void loadElfFile(std::vector<uint8_t> program) {
-  Elf32_Ehdr *ehdr;
-  Elf32_Phdr *phdr;
-  Elf32_Shdr *shdr;
-  func_t f;
+Elf32_Shdr *get_shdr(std::vector<uint8_t > &program, int index) {
+  Elf32_Ehdr *ehdr = get_ehdr(program);
+  if (index < 0 || index >= ehdr->e_shnum) {
+    std::cerr << "Section header " << index << " not found." << std::endl;
+    return(NULL);
+  }
+  Elf32_Shdr *shdr = (Elf32_Shdr *)(program.data() + ehdr->e_shoff + ehdr->e_shentsize * index);
+  return shdr;
+}
 
-  ehdr = (Elf32_Ehdr *) reinterpret_cast<Elf32_Ehdr *>(program.data());
+Elf32_Shdr *search_shdr(std::vector<uint8_t> &program, std::string name) {
+  Elf32_Ehdr *ehdr = get_ehdr(program);
+
+  // Find the last section header that has the name information.
+  Elf32_Shdr *nhdr = get_shdr(program, ehdr->e_shstrndx);
+  for(int i = 0; i < ehdr->e_shnum; i++) {
+    Elf32_Shdr *shdr = get_shdr(program, i);
+    char *section_name = reinterpret_cast<char *>(program.data()) + nhdr->sh_offset + shdr->sh_name;
+    if (!std::strcmp(section_name, name.c_str())) {
+      std::cerr << "Section " << name << " found at " << std::hex
+      << nhdr->sh_offset + shdr->sh_name << "." << std::endl;
+      return shdr;
+    }
+  }
+}
+
+void extend_mem_size(std::vector<uint8_t> &program, int new_size) {
+  if (program.size() < new_size) {
+    new_size = (new_size + kUnitSize - 1) / kUnitSize * kUnitSize;
+    program.resize(new_size);
+    std::cerr << "\nMemory size extended to " << std::hex << new_size << std::endl;
+  }
+}
+
+void loadElfFile(std::vector<uint8_t> &program, std::vector<uint8_t> &memory) {
+  Elf32_Ehdr *ehdr = get_ehdr(program);
   if (isRightElf(ehdr)) {
-    std::cout << "This is an Elf file" << std::endl;
+    std::cerr << "This is an Elf file" << std::endl;
   }
 
   for(int i = 0; i < ehdr->e_phnum; i++) {
-    std::cout << "Program Header " << i << ":";
-    phdr = (Elf32_Phdr *)(program.data() + ehdr->e_phoff + ehdr->e_phentsize * i);
+    std::cerr << "Program Header " << i << ":";
+    Elf32_Phdr *phdr = (Elf32_Phdr *)(program.data() + ehdr->e_phoff + ehdr->e_phentsize * i);
     switch (phdr->p_type) {
+      int total_new_size;
+
       case PT_LOAD:
-        std::cout << "Type: LOAD" << std::endl;
+        std::cerr << "Type: LOAD. ";
+        std::cerr << "Copy to 0x" << std::hex << static_cast<int>(phdr->p_vaddr)
+        << " from 0x" << static_cast<int>(phdr->p_offset) << std::dec
+        << ", size " << static_cast<int>(phdr->p_filesz) << ". ";
+        total_new_size = phdr->p_vaddr + phdr->p_filesz;
+        extend_mem_size(program, total_new_size);
+        for (int i = 0; i < phdr->p_filesz; i++) {
+          memory[phdr->p_vaddr + i] = program[phdr->p_offset];
+        }
+        std::cerr << "Loaded" << std::endl;
         break;
       default:
-        std::cout << "Type: OTHER" << std::endl;
+        std::cerr << "Type: OTHER" << std::endl;
         break;
     }
   }
 
+  // Set up BSS (/
+  Elf32_Shdr *shdr = search_shdr(program, ".bss");
+  if (shdr) {
+    std::cerr << "Secure BSS." << std::endl;
+    int total_new_size = shdr->sh_addr + shdr->sh_size;
+    extend_mem_size(program, total_new_size);
+  }
+}
+
+int get_entry_point(std::vector<uint8_t> &program) {
+  Elf32_Ehdr *ehdr = get_ehdr(program);
+ return ehdr->e_entry;
 }
 
 int main(int argc, char *argv[]) {
@@ -101,8 +157,15 @@ int main(int argc, char *argv[]) {
   }
   std::vector<uint8_t> program = readFile(argv[1]);
 
-  loadElfFile(program);
+  std::vector<uint8_t> memory(kInitialSize);
+  loadElfFile(program, memory);
+  int entry_point = get_entry_point(program);
+  std::cerr << "Entry point is 0x" << std::hex << entry_point << std::endl;
 
+  extend_mem_size(memory, memory.size() + kStackSize);
+  int sp = memory.size() - 4;
+
+  uint8_t mem[4096];
   // Generate very primitive assembly code
   load_assembler_sum(mem);
   printf("Assembler set.\n");
